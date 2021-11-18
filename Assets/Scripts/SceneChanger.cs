ad39a3c9ab66e38b35efad63d283388d1ab1da7e
ufukogutcu <ufukogutcu@hotmail.com>
2021-11-18 23:29:33 +0000

    Added settings and multiplayer buttons

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;  
+
+public class SceneChanger: MonoBehaviour { 
+
+    public void LoadScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void LoadScene(int scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+}   
