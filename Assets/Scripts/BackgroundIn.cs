2a73346eb4ee50142e65e5f830d4b9235c0ad8a5
ufukogutcu <ufukogutcu@hotmail.com>
2021-11-25 18:09:47 +0000

    Fixed resolution and scaling problems

--- Assets/Scripts/BackgroundIn.cs
+++ Assets/Scripts/BackgroundIn.cs
@@ -8,7 +8,7 @@ public class BackgroundIn : MonoBehaviour
 {
 
     [SerializeField]
-    Vector3 target_position,target_scale;
+    Vector2 target_position,target_scale;
     [SerializeField]
     float target_z,rate;
     [SerializeField]
@@ -16,14 +16,15 @@ public class BackgroundIn : MonoBehaviour
     [SerializeField]
     string scene;
 
-    Vector3 position_step,scale_step;
+    Vector2 position_step,scale_step;
     float rotation_step;
 
     // Start is called before the first frame update
     void Start()
     {
-        position_step = (target_position-transform.position)/speed;
-        scale_step = (target_scale-transform.localScale)/speed;
+        RectTransform picture = GetComponent<RectTransform>();
+        position_step = (target_position-picture.anchoredPosition)/speed;
+        scale_step = (target_scale-picture.sizeDelta)/speed;
         rotation_step = (target_z-transform.rotation.eulerAngles.z)/speed;
         InvokeRepeating("Move", 0.0f, rate);
     }
@@ -31,18 +32,20 @@ public class BackgroundIn : MonoBehaviour
     // Update is called once per frame
     void Move()
     {
-        Vector3 position_left = transform.position-target_position;
-        Vector3 scale_left = transform.localScale-target_scale;
+        RectTransform picture = GetComponent<RectTransform>();
+
+        Vector2 position_left = picture.anchoredPosition-target_position;
+        Vector2 scale_left = picture.sizeDelta-target_scale;
         float rotation_left = transform.rotation.eulerAngles.z-target_z;
 
         if(position_left.magnitude<0.001 && scale_left.magnitude<0.001 && Math.Abs(rotation_left)<0.001){
             SceneManager.LoadScene(scene);
         }
         if(position_left.magnitude>0.001){
-        transform.position += (position_step);
+        picture.anchoredPosition += (position_step);
         }
         if(scale_left.magnitude>0.001){
-        transform.localScale += scale_step;
+        picture.sizeDelta += scale_step;
         }
         if(Math.Abs(rotation_left)>0.001){
         transform.Rotate(0, 0, rotation_step, Space.Self);
