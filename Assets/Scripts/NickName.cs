8bfa9ef97afa7caea659076c7785c467c79da13d
ufukogutcu <ufukogutcu@hotmail.com>
2021-11-26 14:25:32 +0000

    Nickname panel

---
+++ Assets/Scripts/NickName.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NickName : MonoBehaviourPunCallbacks
+{
+
+    public GameObject panel;
+
+    public void setNickname(InputField code){
+        string name;
+        
+        if(code.text==""){
+            name="Player";
+        }else{
+            name=code.text;
+        }
+        PhotonNetwork.LocalPlayer.NickName = name;
+        Debug.Log("Nickname set");
+        Destroy(panel);
+
+    }
+}
