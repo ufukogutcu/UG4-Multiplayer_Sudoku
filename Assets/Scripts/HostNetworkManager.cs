c8be8ed48eff28352a95d25805b6d30180aaf167
ufukogutcu <ufukogutcu@hotmail.com>
2021-11-26 13:51:28 +0000

    Created Multiplayer Room Scene

---
+++ Assets/Scripts/HostNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class HostNetworkManager : MonoBehaviourPunCallbacks
+{
+    public void GoBack()
+    {
+
+        LeaveRoom();
+        SceneManager.LoadScene("MultiplayerMenu");
+
+    }
+
+    public void LeaveRoom()
+    {
+
+        PhotonNetwork.LeaveRoom();
+        Debug.Log("Left lobby");
+
+    }
+}
