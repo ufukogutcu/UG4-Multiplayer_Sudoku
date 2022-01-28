15e1384ae90a878c902541cb25d6b6f60b6abd4a
ufukogutcu <ufukogutcu@hotmail.com>
2022-01-28 02:29:18 +0000

    Single:Cells lighting up

--- Assets/Scripts/CellData.cs
+++ Assets/Scripts/CellData.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CellData : MonoBehaviour
 {
     public int current = 0;
     public int answer = 0;
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "LeftNumbers")
+            gameObject.GetComponent<Image>().color = new Color32(82,82,62,103);
+    }
+
+    void OnCollisionExit2D(Collision2D col) {
+ 
+        if (col.gameObject.tag == "LeftNumbers")
+            gameObject.GetComponent<Image>().color = new Color32(82,82,62,0);
+    }
 }
