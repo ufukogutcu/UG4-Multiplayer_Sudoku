2eaf995d7c0a52d7f1fecc744e7ca9a9e52c4e3a
ufukogutcu <ufukogutcu@hotmail.com>
2022-01-28 00:29:51 +0000

    Singleplayer: Buttons on the left

---
+++ Assets/Scripts/BoardCreator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardCreator : MonoBehaviour
+{
+
+    void generate_board()
+    {
+        int a = 1;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
