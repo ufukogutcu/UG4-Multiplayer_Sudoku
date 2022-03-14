a7d7ecf6603786544c0f0bb93db42e5cff0d700f
ufukogutcu <ufukogutcu@hotmail.com>
2022-03-14 01:57:24 +0000

    Sabotage box prefab

---
+++ Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(Settings_sabotage.sabotagelength);
+        Destroy(gameObject);
+    }
+}
